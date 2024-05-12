27c4abcdc17f1c576ce1d789209027aea239baaa
Mansadhit301 <aadhit407@gmail.com>
2024-05-12 23:14:33 +0800

    feat: add userId foreign key in posts

--- database/migrations/2024_05_10_142703_create_posts_table.php
+++ database/migrations/2024_05_10_142703_create_posts_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->text("excerpt");
             $table->text("body");
             $table->foreignId("category_id");
+            $table->foreignId("user_id");
             $table->timestamp("published_at")->nullable();
             $table->timestamps();
         });
